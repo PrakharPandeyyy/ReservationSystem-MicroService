8e8c004dac0b86f2927b255a4a75eaf2fc692c58
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-11-23 18:33:24 +0530

    added auth microservice

---
+++ apps/auth/src/users/users.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common';
+import { UserDocument, UserSchema } from './schema/user.schema';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UsersController, UsersRepository],
+  providers: [UsersService],
+})
+export class UsersModule {}
